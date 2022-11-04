e662142e984d2b9d7d1d1a70412da0216a26e879
ebr <clkebr2601@gmail.com>
2022-11-04 15:04:02 +0100

    fixed insertUser

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +13,18 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceImpl implements UserService{
 
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
 
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+       List<User> userList = userRepository.findAll();
+       return userList.stream().map(userMapper::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
@@ -23,7 +34,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void save(UserDTO dto) {
-
+        userRepository.save(userMapper.convertToEntity(dto));
     }
 
     @Override
